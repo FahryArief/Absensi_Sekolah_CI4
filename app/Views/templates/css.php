549f37253804481c15d33466c97f1e5479509837
Muhammad Fahry Arief Billah <100301880+FahryArief@users.noreply.github.com>
2023-08-31 09:10:52 +0700

    First Commit

---
+++ app/Views/templates/css.php
@@ -0,0 +1,10 @@
+<link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('public/assets/img/apple-icon.png'); ?>">
+<link rel="icon" type="image/png" href="<?= base_url('public/assets/img/favicon.png'); ?>">
+
+<!-- CSS Files -->
+<link href="<?= base_url('public/assets/fonts/fonts.css'); ?>" rel="stylesheet" />
+<link href="<?= base_url('public/assets/css/material-dashboard.css'); ?>" rel="stylesheet" />
+<link href="<?= base_url('public/assets/css/fontawesome.min.css'); ?>" rel="stylesheet">
+
+<!-- CSS Just for demo purpose, don't include it in your project -->
+<link href="<?= base_url('public/assets/demo/demo.css'); ?>" rel="stylesheet" />
